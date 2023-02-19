7ad4b66d33c4e83628d36299d28084d0577316ff
Richy <richymbiamou52@gmail.com>
2023-02-19 06:06:51 +0100

    realisation de la section 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'partytrick';
+  ngOnInit(){
+    // Activation ou Desactivation des li
+    let nav_li = document.querySelectorAll('nav li')
+    // @ts-ignore
+    nav_li.forEach(li => li.addEventListener('click', () =>{
+      nav_li.forEach(all_li => all_li.classList.remove('active'))
+      li.classList.add('active')
+    }))
+    // @ts-ignore
+    var swiper = new Swiper(".mySwiper", {
+      effect: "cards",
+      grabCursor: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      loop:true,
+      centeredSlides: true,
+      slidesPerView: 2,
+    });
+  }
+  dropdown(){
+    // @ts-ignore
+    let drop = document.querySelector('.dropdown');
+    // @ts-ignore
+    if (drop.style.display=='none'){
+      // @ts-ignore
+      drop.style.display='block';
+    }
+    else {
+      // @ts-ignore
+      drop.style.display='none'
+    }
+  }
 }
